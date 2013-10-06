41c1d074b5277888b4d2c0d282d8e8961ca06a7d
CaFa <cafacafacafa@gmail.com>
2013-10-06 14:06:21 +0400

    1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
+/* Window */
+	$(".window-close").on("click", function() {
+		$(".window").removeClass("open");
+	});
+	$(".window").on("click", function(e) {
+	    e.stopPropagation();
+	});
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) { 
+			$(".window").removeClass("open");
+		}
+	});
+	
+/* Menu auth */
+$("#menu-auth").on("click", function() {
+	$("#window-auth").addClass("open");
+});
+
+/* Menu */
 $(".menu-node").on("click", function(e) {
     $(".menu-node").removeClass("open");
     $(this).addClass("open");
 });
+
